82ed704bb93a38c0f0320e014c2b668b3074085d
Re-Dei <hridaya.pradhan123@gmail.com>
2022-02-05 20:20:52 +0545

    added documentation

--- CarRent/loginadmin.cpp
+++ CarRent/loginadmin.cpp
@@ -13,6 +13,7 @@ LoginAdmin::LoginAdmin(QWidget *parent)
         qDebug()<<"Connected...";
         hintLoginDetails();
     }else{
+        /*display error if cannot connect to the database*/
         qDebug()<<"Cannot connect to database";
         QMessageBox::critical(this, "Error", "Cannot connect to the database.");
         QCoreApplication::exit();
@@ -24,16 +25,26 @@ LoginAdmin::~LoginAdmin()
     delete ui;
 }
 
+
 /**
- * @brief LoginAdmin::hintLoginDetails
+ * @brief uses the empty labels in the login page to hint the default login details
  *
- * uses the empty labels in the login page to hint the default login details
+ * gets total number of accounts from getTotalAccounts()
+ * checks if database is corrupted
+ * if yes, sends an error message to user and terminates program
+ * if not, checks if there is only one account in the database
+ * if yes, shows default login details
  */
 void LoginAdmin::hintLoginDetails()
 {
-    /*if there is only one account in the table accounts in the database*/
-    if (admin.getTotalAccounts() == 1){
-        /*hint the user default login details*/
+    int x = admin.getTotalAccounts();
+    /*check if database is corrupted*/
+    if (x == 0){
+        /*send an error message to user and terminates program*/
+        QMessageBox::critical(this, "Error", "Database is corrupted");
+        QCoreApplication::exit();
+    }else if(x == 1){
+        /*if there is only one account in the table accounts in the database, hint the user default login details*/
         ui->label_hintUsername->setText("Hint: useradmin");
         ui->label_hintPassword->setText("Hint: password");
     }
