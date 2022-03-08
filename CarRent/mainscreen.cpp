801dc523055b188e79537d5aba6bfc1e2fc1e5ae
Re-Dei <hridaya.pradhan123@gmail.com>
2022-03-08 20:32:05 +0545

    made function to validate rate

--- CarRent/mainscreen.cpp
+++ CarRent/mainscreen.cpp
@@ -25,26 +25,39 @@ MainScreen::~MainScreen()
     delete ui;
 }
 
-
-
-
-
-
+/**
+ * @brief when text is edited on the line edit where the rate is entered, checks if the entered rate is valid
+ * @param arg1
+ *
+ * initially sets isValid as true which represents the rate entered is valid
+ * loops to check each character of the string entered by the user
+ * if the string is not valid (is not a digit), isValid is changed to false and the loop is broken
+ * then check if isValid is true or false
+ * if isValid is false then disables the add button on the ui of add->car and warns the user
+ * if isValid is true then enables the add button on the ui and removes the warning
+ */
 void MainScreen::on_lineEdit_rate_textEdited(const QString &arg1)
 {
-    bool isValid = true;
+    bool isValid = true;   //true if the entered rate is valid
+
+    /*loop to check each character of the string*/
     for(int i = 0; i < arg1.length(); i++)
     {
+        /*check if the character is digit*/
         if (!arg1[i].isDigit()){
+            /*if not, change isValid to false and break the loop*/
             isValid = false;
             break;
         }
     }
 
+    /*check if the rate is not valid*/
     if(!isValid){
+        /*disable the add button on the ui and warn the user*/
         ui->pushButton_addCar->setEnabled(false);
         ui->label_warnRate->setText("<font color='red'>Enter a valid rate");
     }else{
+        /*else enable the add button in ui and remove the warning*/
         ui->pushButton_addCar->setEnabled(true);
         ui->label_warnRate->setText("");
     }
@@ -58,5 +71,6 @@ void MainScreen::on_pushButton_addCar_clicked()
     ThisCar.Brand = ui->lineEdit_brand->text();
     ThisCar.Model = ui->lineEdit_model->text();
     ThisCar.Rate = ui->lineEdit_rate->text().toInt();
+    ThisCar.isAvailable = true;
 }
 
